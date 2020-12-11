f72e45894e43ecf83b38be78a08d21ddc589d366
DjDeveloperr <djsandhu2005@gmail.com>
2020-12-11 11:09:13 +0530

    clean up

--- bot.ts
+++ bot.ts
@@ -1,98 +1,37 @@
-import { Client, Intents, event, slash, Interaction, InteractionResponseType, ClientOptions, SlashCommandPartial, SlashCommandOptionType } from './deps.ts'
+import {
+    Client,
+    Intents,
+    event,
+    slash,
+    Interaction,
+    InteractionResponseType,
+    ClientOptions,
+} from "./deps.ts";
 import { MAX_USER_TAGS, TOKEN } from "./config.ts";
-import { addTag, deleteTag, editTag, getGuildTags, getTag, getUserTags, init } from "./db.ts";
+import {
+    addTag,
+    deleteTag,
+    editTag,
+    getGuildTags,
+    getTag,
+    getUserTags,
+    init,
+} from "./db.ts";
+import { commands } from "./commands.ts"
 
-console.log('Init DB');
+console.log("Init DB");
 init();
 
-const commands: SlashCommandPartial[] = [];
-
-commands.push({
-    name: 'mytags',
-    description: 'See a list of tags made by you!',
-    options: []
-})
-
-commands.push({
-    name: 'alltags',
-    description: 'See a list of tags in this server!',
-    options: []
-})
-
-commands.push({
-    name: 'addtag',
-    description: 'Create a new tag in this server!',
-    options: [
-        {
-            name: 'name',
-            description: 'Name of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        },
-        {
-            name: 'content',
-            description: 'New content of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        }
-    ]
-})
-
-commands.push({
-    name: 'updatetag',
-    description: 'Update your tag\'s response.',
-    options: [
-        {
-            name: 'name',
-            description: 'Name of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        },
-        {
-            name: 'content',
-            description: 'New content of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        }
-    ]
-})
-
-commands.push({
-    name: 'deletetag',
-    description: 'Delete a tag of yours.',
-    options: [
-        {
-            name: 'name',
-            description: 'Name of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        }
-    ]
-})
-
-commands.push({
-    name: 'tag',
-    description: 'Send a tag\'s contents.',
-    options: [
-        {
-            name: 'name',
-            description: 'Name of the tag.',
-            required: true,
-            type: SlashCommandOptionType.STRING
-        }
-    ]
-})
-
 interface MyClientOptions extends ClientOptions {
-    syncCommands?: boolean
+    syncCommands?: boolean;
 }
 
 class MyClient extends Client {
-    syncCommands: boolean = false
+    syncCommands: boolean = false;
 
     constructor(options?: MyClientOptions) {
-        super(options)
-        if(options?.syncCommands === true) this.syncCommands = true
+        super(options);
+        if (options?.syncCommands === true) this.syncCommands = true;
     }
 
     @event()
@@ -100,144 +39,178 @@ class MyClient extends Client {
         console.log(`Logged in as ${this.user?.tag}!`);
         if (this.syncCommands) {
             console.log(`Syncing commands...`);
-            commands.forEach(cmd => {
-                this.slash.commands.create(cmd).then(c => {
-                    console.log(`Created CMD ${cmd.name}!`);
-                }).catch(() => console.log(`Failed to create ${cmd.name}.`));
+            commands.forEach((cmd) => {
+                this.slash.commands
+                    .create(cmd)
+                    .then((c) => {
+                        console.log(`Created CMD ${cmd.name}!`);
+                    })
+                    .catch(() => console.log(`Failed to create ${cmd.name}.`));
             });
         }
     }
 
     @slash()
     mytags(d: Interaction) {
-        const tags = getUserTags(d.guild.id, d.user.id)
-        if (tags.length == 0) return d.respond({ content: `You have no tags in this server yet.`, temp: true, type: InteractionResponseType.CHANNEL_MESSAGE })
+        const tags = getUserTags(d.guild.id, d.user.id);
+        if (tags.length == 0)
+            return d.respond({
+                content: `You have no tags in this server yet.`,
+                temp: true,
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+            });
         else {
             d.respond({
                 type: InteractionResponseType.CHANNEL_MESSAGE,
                 temp: true,
-                content: `**Your Tags:** ${tags.map(e => e.name).join(', ')}`
+                content: `**Your Tags:** ${tags.map((e) => e.name).join(", ")}`,
             });
         }
     }
 
     @slash()
     alltags(d: Interaction) {
-        const tags = getGuildTags(d.guild.id)
-        if (tags.length == 0) return d.respond({ content: `This server has no tags yet.`, temp: true, type: InteractionResponseType.CHANNEL_MESSAGE })
+        const tags = getGuildTags(d.guild.id);
+        if (tags.length == 0)
+            return d.respond({
+                content: `This server has no tags yet.`,
+                temp: true,
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+            });
         else {
             d.respond({
                 type: InteractionResponseType.CHANNEL_MESSAGE,
                 temp: true,
-                content: `**All Tags:** ${tags.map(e => e.name).join(', ')}`
-            })
+                content: `**All Tags:** ${tags.map((e) => e.name).join(", ")}`,
+            });
         }
     }
 
     @slash()
     addtag(d: Interaction) {
         let tags = getUserTags(d.guild.id, d.user.id);
-        if(tags.length >= MAX_USER_TAGS) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `You have reached maximum number of tags!`,
-            temp: true,
-        })
-
-        let name = d.data.options.find(e => e.name == 'name')?.value as string;
-        let content = d.data.options.find(e => e.name == 'content')?.value as string;
-
-        if(content.length > 2000) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag content length must be between 1-2000 characters.`,
-            temp: true,
-        })
-
-        let added = addTag(d.guild.id, d.user.id, name, content);
-        if(added == null) {
+        if (tags.length >= MAX_USER_TAGS)
             return d.respond({
                 type: InteractionResponseType.CHANNEL_MESSAGE,
-                content: `Tag with name \`${name.replace(/`/g, "\`")}\` already exists.`,
+                content: `You have reached maximum number of tags!`,
                 temp: true,
-            })
-        } else return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Added new tag: \`${name.replace(/`/g, "\`")}\`!`,
-            temp: true,
-        })
+            });
+
+        let name = d.data.options.find((e) => e.name == "name")?.value as string;
+        let content = d.data.options.find((e) => e.name == "content")
+            ?.value as string;
+
+        if (content.length > 2000)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag content length must be between 1-2000 characters.`,
+                temp: true,
+            });
+
+        let added = addTag(d.guild.id, d.user.id, name, content);
+        if (added == null) {
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(/`/g, "`")}\` already exists.`,
+                temp: true,
+            });
+        } else
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Added new tag: \`${name.replace(/`/g, "`")}\`!`,
+                temp: true,
+            });
     }
 
     @slash()
     updatetag(d: Interaction) {
-        let name = d.data.options.find(e => e.name == 'name')?.value as string;
-        let content = d.data.options.find(e => e.name == 'content')?.value as string;
+        let name = d.data.options.find((e) => e.name == "name")?.value as string;
+        let content = d.data.options.find((e) => e.name == "content")
+            ?.value as string;
 
-        if(content.length > 2000) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag content length must be between 1-2000 characters.`,
-            temp: true,
-        })
+        if (content.length > 2000)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag content length must be between 1-2000 characters.`,
+                temp: true,
+            });
 
-        let tag = getTag(d.guild.id, name)
-        if(!tag) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag with name \`${name.replace(/`/g, "\`")}\` could not be found.`,
-            temp: true,
-        })
-        if(tag.user != d.user.id) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag with name \`${name.replace(/`/g, "\`")}\` is not your!`,
-            temp: true,
-        })
+        let tag = getTag(d.guild.id, name);
+        if (!tag)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(
+                    /`/g,
+                    "`"
+                )}\` could not be found.`,
+                temp: true,
+            });
+        if (tag.user != d.user.id)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(/`/g, "`")}\` is not your!`,
+                temp: true,
+            });
 
-        editTag(d.guild.id, name, content)
+        editTag(d.guild.id, name, content);
         d.respond({
             type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Updated tag: \`${name.replace(/`/g, "\`")}\`!`,
+            content: `Updated tag: \`${name.replace(/`/g, "`")}\`!`,
             temp: true,
-        })
+        });
     }
 
     @slash()
     deletetag(d: Interaction) {
-        let name = d.data.options.find(e => e.name == 'name')?.value as string;
+        let name = d.data.options.find((e) => e.name == "name")?.value as string;
         const tag = getTag(d.guild.id, name);
-        if (!tag) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag with name \`${name.replace(/`/g, "\`")}\` could not be found.`,
-            temp: true,
-        })
-        if(tag.user != d.user.id) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag with name \`${name.replace(/`/g, "\`")}\` is not your!`,
-            temp: true,
-        })
+        if (!tag)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(
+                    /`/g,
+                    "`"
+                )}\` could not be found.`,
+                temp: true,
+            });
+        if (tag.user != d.user.id)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(/`/g, "`")}\` is not your!`,
+                temp: true,
+            });
 
-        deleteTag(d.guild.id, name)
+        deleteTag(d.guild.id, name);
         d.respond({
             type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Delete tag: \`${name.replace(/`/g, "\`")}\`!`,
+            content: `Delete tag: \`${name.replace(/`/g, "`")}\`!`,
             temp: true,
-        })
+        });
     }
 
     @slash()
     tag(d: Interaction) {
-        let name = d.data.options.find(e => e.name == 'name')?.value as string;
+        let name = d.data.options.find((e) => e.name == "name")?.value as string;
         const tag = getTag(d.guild.id, name);
-        if (!tag) return d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE,
-            content: `Tag with name \`${name.replace(/`/g, "\`")}\` could not be found.`,
-            temp: true,
-        })
-        else d.respond({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            content: tag.content.replace(/@everyone/g, '@everyоne'),
-        })
+        if (!tag)
+            return d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE,
+                content: `Tag with name \`${name.replace(
+                    /`/g,
+                    "`"
+                )}\` could not be found.`,
+                temp: true,
+            });
+        else
+            d.respond({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                content: tag.content.replace(/@everyone/g, "@everyоne"),
+            });
     }
 }
 
 export const client = new MyClient({
-    syncCommands: true
+    syncCommands: true,
 });
 
 if (import.meta.main) {
